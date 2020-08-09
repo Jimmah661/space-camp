9b17f5d5f713c8ec79513f876e6138cb712b8a03
James Rudd <james.rudd45@gmail.com>
2020-08-09 08:21:13 +0800

    Updates

---
+++ precache-manifest.293dcb67b7efa09b2cc1d416bbd2a1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3397a996f831ca6486d5b200a4fdfccd",
+    "url": "/space-camp/index.html"
+  },
+  {
+    "revision": "ce8485eebdc5cec4712d",
+    "url": "/space-camp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0be472f522e40f7906e",
+    "url": "/space-camp/static/js/2.623d7c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/space-camp/static/js/2.623d7c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8485eebdc5cec4712d",
+    "url": "/space-camp/static/js/main.ec6f9749.chunk.js"
+  },
+  {
+    "revision": "c5ead80da10d7c73e1c2",
+    "url": "/space-camp/static/js/runtime-main.3116a03b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/space-camp/static/media/logo.5d5d9eef.svg"
+  }
+]);
